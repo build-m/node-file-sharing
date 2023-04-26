65e28b012e13300cb82ec95851b31d9d402bde61
build-m <godolyas123@gmail.com>
2023-04-26 09:19:14 -0700

    script-done

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,13 @@ const form = document.querySelector("#form")
 const inputFile = document.querySelector("#file")
 const uploadFile = document.querySelector(".upload-file")
 const uploaded = document.querySelector(".uploaded")
+
+form.onclick = e => {
+            inputFile.click();
+        }
+
+        inputFile.onchange = e => {
+            // console.log(e.target.files)      
+            const filename = e.target.files[0].name
+             console.log(filename)
+        }
