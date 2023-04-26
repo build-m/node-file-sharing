122912e22756a6e7a9bd2729819ea79da47a8570
build-m <godolyas123@gmail.com>
2023-04-26 09:04:09 -0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("#form")
+const inputFile = document.querySelector("#file")
+const uploadFile = document.querySelector(".upload-file")
+const uploaded = document.querySelector(".uploaded")
